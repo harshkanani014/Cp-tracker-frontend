63125d0ab48a739185a49b0119d0f3a15c6f7e92
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-20 17:52:52 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/th3c0d3br34ker.github.io/CP-Tracker-Frontend/precache-manifest.029c395ee5f196f7aa01466351db5445.js"
+  "/CP-Tracker-Frontend/precache-manifest.32a06813987c9f3ace9fd0e1801d3e99.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/th3c0d3br34ker.github.io/CP-Tracker-Frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CP-Tracker-Frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
